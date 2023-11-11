8521ac286f33518031853c160bf20406ef9a04df
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-11-11 12:35:11 +0400

    Finished validation

--- app/src/main/java/com/example/resetpassword/MainActivity.kt
+++ app/src/main/java/com/example/resetpassword/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.resetpassword
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import java.util.regex.Pattern
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,19 +25,31 @@ class MainActivity : AppCompatActivity() {
         resetButton = findViewById(R.id.button)
 
 
-        resetButton.setOnClickListener() {
-            if (isValidPhoneNumber(phone.text.toString())) {
-                Toast.makeText(this,"აისაღოლ", Toast.LENGTH_LONG).show()
-            }else {
+        resetButton.setOnClickListener {
+            if (isValidPhoneNumber(phone.text.toString()) && isValidSMSCode(smsCode.text.toString()) && isValidPassword(resetPassword.text.toString(), repeatPassword.text.toString())) {
+                Toast.makeText(this,"ახალ პაროლს მიიღებთ SMS-ით.", Toast.LENGTH_LONG).show()
+            }
+            else {
                 Toast.makeText(this,"ვერააააააააააააა", Toast.LENGTH_LONG).show()
             }
         }
-
     }
 
     private fun isValidPhoneNumber(phone: String): Boolean {
         val regex = "^\\d{9}\$".toRegex()
         return phone.matches(regex)
+    }
+    private fun isValidSMSCode(smsCode: String): Boolean {
+        val regex = "^\\d{4}\$".toRegex()
+        return smsCode.matches(regex)
+    }
+    private fun isValidPassword(password: String, repeatPassword: String): Boolean {
+        val regex = "^(?=.*\\d)[a-zA-Z\\d]{8}\$".toRegex()
+        return if (password == repeatPassword) {
+            password.matches(regex)
+        }else {
+            false
+        }
     }
 
 }
