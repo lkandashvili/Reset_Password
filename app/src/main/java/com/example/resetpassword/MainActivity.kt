7de7707a4b0fd5eb18c6754f9c9b27e8e3b58c60
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-11-11 12:18:07 +0400

    added phone number validation

--- app/src/main/java/com/example/resetpassword/MainActivity.kt
+++ app/src/main/java/com/example/resetpassword/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.resetpassword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var phone: EditText
+    private lateinit var smsCode: EditText
+    private lateinit var resetPassword: EditText
+    private lateinit var repeatPassword: EditText
+    private lateinit var resetButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        phone = findViewById(R.id.editTextPhone)
+        smsCode = findViewById(R.id.editTextNumber)
+        resetPassword = findViewById(R.id.editTextTextPassword)
+        repeatPassword = findViewById(R.id.editTextTextPassword2)
+        resetButton = findViewById(R.id.button)
+
+
+        resetButton.setOnClickListener() {
+            if (isValidPhoneNumber(phone.text.toString())) {
+                Toast.makeText(this,"აისაღოლ", Toast.LENGTH_LONG).show()
+            }else {
+                Toast.makeText(this,"ვერააააააააააააა", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
+
+    private fun isValidPhoneNumber(phone: String): Boolean {
+        val regex = "^\\d{9}\$".toRegex()
+        return phone.matches(regex)
+    }
+
 }
